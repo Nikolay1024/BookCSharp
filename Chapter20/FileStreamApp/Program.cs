bb8936b49470383e569d1c0ce6b899a83b3c38a5
Nikolay <zorinov.nikolai@yandex.ru>
2021-06-05 18:41:58 +0300

    Работа с классом FileStream

---
+++ Chapter20/FileStreamApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileStreamApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=> Работа с классом FileStream");
+            // Получить объект FileStream.
+            using (FileStream fileStream = File.Open("Message.txt", FileMode.Create))
+            {
+                string msg = "Hello!";
+                Console.WriteLine("Исходное сообщение: {0}", msg);
+
+                // Закодировать строку в byte[].
+                byte[] msgAsByteArray = Encoding.Default.GetBytes(msg);
+                // Записать byte[] в файл.
+                fileStream.Write(msgAsByteArray, 0, msgAsByteArray.Length);
+                
+                // Сбросить внутреннюю позицию потока.
+                fileStream.Position = 0;
+
+                byte[] bytesFromFile = new byte[msgAsByteArray.Length];
+                // Прочитать byte[] из файла.
+                fileStream.Read(bytesFromFile, 0, msgAsByteArray.Length);
+                Console.Write("Ваше сообщение как массив байтов: ");
+                Console.WriteLine(string.Join(" ", bytesFromFile));
+
+                Console.Write("Декодированное сообщение: ");
+                // Декодировать строку из byte[].
+                Console.WriteLine(Encoding.Default.GetString(bytesFromFile));
+            }
+            Console.ReadLine();
+        }
+    }
+}
