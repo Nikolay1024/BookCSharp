ce3b875fd4bddbc720fadb1f1717390d0d15aee5
Nikolay <zorinov.nikolai@yandex.ru>
2021-06-06 14:54:51 +0300

    Работа с классами StringWriter И StringReader

---
+++ Chapter20/StreamWriterReaderApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace StreamWriterReaderApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamWriterReader1(); // Более высокая гибкость управления чтением/записью
+            StreamWriterReader2();
+            Console.ReadLine();
+        }
+
+        static void StreamWriterReader1()
+        {
+            Console.WriteLine("=> Работа с классами StreamWriter и StreamReader");
+            // Получить объект StreamWriter и записать строковые данные.
+            using (StreamWriter streamWriter = File.CreateText("Message1.txt"))
+            {
+                streamWriter.WriteLine("Числа:");
+                streamWriter.Write(string.Join(" ", Enumerable.Range(0, 10)));
+                streamWriter.Write(streamWriter.NewLine);
+            }
+            // Прочитать данные из файла.
+            Console.WriteLine("Содержимое файла Message.txt:");
+            using (StreamReader streamReader = File.OpenText("Message1.txt"))
+            {
+                string input = null;
+                while ((input = streamReader.ReadLine()) != null)
+                    Console.WriteLine(input);
+            }
+        }
+        static void StreamWriterReader2()
+        {
+            Console.WriteLine("=> Работа с классами StreamWriter и StreamReader");
+            // Получить объект StreamWriter и записать строковые данные.
+            using (StreamWriter streamWriter = new StreamWriter("Message2.txt"))
+            {
+                streamWriter.WriteLine("Числа:");
+                streamWriter.Write(string.Join(" ", Enumerable.Range(0, 10)));
+                streamWriter.Write(streamWriter.NewLine);
+            }
+            // Прочитать данные из файла.
+            Console.WriteLine("Содержимое файла Message.txt:");
+            using (StreamReader streamReader = new StreamReader("Message2.txt"))
+            {
+                string input = null;
+                while ((input = streamReader.ReadLine()) != null)
+                    Console.WriteLine(input);
+            }
+        }
+    }
+}
---
+++ Chapter20/StringReaderWriterApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StringReaderWriterApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=> Работа с классами StringWriter И StringReader");
+            // Создать объект StringWriter и записать символьные данные в память.
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                stringWriter.WriteLine("Строка");
+                // Получить копию содержимого (хранящегося в строке) и вывести на консоль.
+                Console.Write("StringWriter: {0}", stringWriter);
+
+                // Получить внутренний объект StringBuilder.
+                StringBuilder stringBuilder = stringWriter.GetStringBuilder();
+                stringBuilder.Insert(0, "Привет! ");
+                Console.Write("StringBuilder после Insert: {0}", stringBuilder);
+                stringBuilder.Remove(0, "Привет! ".Length);
+                Console.Write("StringBuilder после Remove: {0}", stringBuilder);
+
+                using (StringReader stringReader = new StringReader(stringWriter.ToString()))
+                {
+                    string input = null;
+                    while ((input = stringReader.ReadLine()) != null)
+                        Console.WriteLine(input);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
