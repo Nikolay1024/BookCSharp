6c50f33cbfa000f8203e8a5edb0b88e3094b09f9
Nikolay <zorinov.nikolai@yandex.ru>
2022-06-26 23:04:49 +0300

    WCF default bindings

---
+++ Chapter23/MagicEightBallServiceHTTPDefaultBindings/MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MagicEightBallServiceLib
+{
+    public class MagicEightBallService : IEightBall
+    {
+        // Просто для отображения на хосте.
+        public MagicEightBallService()
+        {
+            Console.WriteLine("8-Ball ожидает твоего вопроса...");
+        }
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers = { "Будущее неопределенно", "Да", "Нет", "Туманно", "Спросите позже", "Определенно" };
+            // Возвратить случайный ответ.
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+        }
+    }
+}
