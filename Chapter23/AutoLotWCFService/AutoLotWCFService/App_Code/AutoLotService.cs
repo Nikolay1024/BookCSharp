770ac1a774df397344f76abe15f920fe1b26955f
Николай <zorinov.nikolai@yandex.ru>
2023-06-08 22:45:52 +0300

    Chapter23: AutoLotWCFService

---
+++ Chapter23/AutoLotWCFService/AutoLotWCFService/App_Code/AutoLotService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AutoLotDataAccessLayer.Models;
+using AutoLotDataAccessLayer.Repos;
+using AutoMapper;
+
+public class AutoLotService : IAutoLotService
+{
+    public AutoLotService()
+    {
+        Mapper.Initialize(cfg => cfg.CreateMap<Inventory, InventoryRecord>());
+    }
+
+    public void InsertCar(string make, string color, string petname)
+    {
+        var repo = new InventoryRepo();
+        repo.Add(new Inventory { Color = color, Make = make, Name = petname });
+        repo.Dispose();
+    }
+
+    public void InsertCar(InventoryRecord car)
+    {
+        var repo = new InventoryRepo();
+        repo.Add(new Inventory { Color = car.Color, Make = car.Make, Name = car.PetName });
+        repo.Dispose();
+    }
+
+    public List<InventoryRecord> GetInventory()
+    {
+        var repo = new InventoryRepo();
+        List<Inventory> records = repo.GetAll();
+        List<InventoryRecord> results = Mapper.Map<List<InventoryRecord>>(records);
+        repo.Dispose();
+        return results;
+    }
+}
---
+++ Chapter23/AutoLotWCFService/AutoLotWCFService/App_Code/IAutoLotService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Реструктуризация" можно использовать для одновременного изменения имени интерфейса "IService" в коде и файле конфигурации.
+[ServiceContract]
+public interface IAutoLotService
+{
+    [OperationContract]
+    void InsertCar(string make, string color, string petname);
+
+    [OperationContract(Name = "InsertCarWithDetails")]
+    void InsertCar(InventoryRecord car);
+
+    [OperationContract]
+    List<InventoryRecord> GetInventory();
+}
+
+
+// Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
